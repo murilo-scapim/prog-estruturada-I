0e0ad771f6d0c7d22093b4355b900c58f267670d
murilo-scapim <mhscapim@gmail.com>
2023-02-28 15:45:11 -0300

    Aula 1

---
+++ Aula1.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+
+# Aqui é um comentário de linha
+
+# Variável é uma alocação de memória para armazenar dados
+# Toda variável possui um tipo de dados
+
+# Python é case sensitive
+
+idade = 30  # inteiro ou int
+peso = 75.50  # ponto flutuante ou float
+nome = 'Murilo'  # string
+ativo = True  # booleano
+
+# entrada, processamento e saída
+Peso = input('Informe seu peso: ')  # input comando de entrada
+altura = input('Informe sua altura: ')
+
+# operadores aritméticos + - * / // ** %
+# imc = Peso / (altura * altura)
+imc = Peso / (altura ** 2)  # processamento
+print(imc)
