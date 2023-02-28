34a5dbbd49c88987b4d6aa58a17f0be67501868b
murilo-scapim <mhscapim@gmail.com>
2023-02-28 15:45:57 -0300

    Aula 27/02

---
+++ Aula2.py
@@ -0,0 +1,7 @@
+# Python é uma linguagem fortemente tipada
+
+peso = float(input('Qual o seu peso? '))  # casting
+altura = float(input('Qual a sua altura? '))
+
+imc = peso / (altura * altura)
+print(imc)
