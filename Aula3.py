5ac0430b162689fa9b98d55cc4b0aabea85c78fc
Murilo Scapim <mhscapim@gmail.com>
2023-03-07 09:38:56 -0300

    Aula 06/03

---
+++ Aula3.py
@@ -0,0 +1,16 @@
+fruta = 'maça'
+print(type(fruta))  # tipo de dados da variável
+
+frutas = ['maça', 'laranja', 'kiwi', 'melão']
+print(type(frutas))
+
+print(frutas[0])  # primeiro elemento
+print(frutas[1])
+# print(frutas[4]) # fora do intervalo de índices
+
+print(len(frutas))  # tamanho do vetor, length
+
+print(frutas[-1])
+
+frutas[0] = 'uva'
+print(frutas)
