6a01d073b90e560bedeff107bcccc2cfd1e6d930
Murilo Scapim <mhscapim@gmail.com>
2023-03-07 22:01:48 -0300

    Aula 07/03

--- Aula3.py
+++ Aula3.py
@@ -12,5 +12,21 @@ print(len(frutas))  # tamanho do vetor, length
 
 print(frutas[-1])
 
-frutas[0] = 'uva'
+# frutas[0] = 'uva'
 print(frutas)
+
+print(frutas[1:3])  # o elem do índice 3 não é incluido
+
+print(frutas[:3])  # não inclue o elem do índice 3
+print(frutas[2:])  # começa no elem de índice 2 até o último
+
+frutas.append('laranja')  # inserção no final
+
+frutas.insert(0, 'uva')
+
+frutas.remove('maça')  # remoção de um elem específico
+
+frutas.pop()  # remoção no final
+frutas.pop(0)  # remoção passando um índice
+
+frutas.clear()  # esvazia a lista
