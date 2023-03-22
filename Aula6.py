99c8c1057a48a731dc7f43d32ed725088ace846f
Murilo Scapim <mhscapim@gmail.com>
2023-03-22 12:10:11 -0300

    Aula 21/03

--- Aula6.py
+++ Aula6.py
@@ -27,3 +27,33 @@ imcs = [(1, 'Ana', 23.5), (2, 'João', 18.5),
 peso_normal = [e for e in imcs
                if e[2] >= 18.5 and e[2] <= 24.99]
 print(peso_normal)
+
+for cod, nome, imc in imcs:
+    print(cod, nome, imc)
+
+peso_normal2 = [nome for cod, nome, imc in imcs
+                if imc >= 18.50 and imc <= 24.99]
+print(peso_normal2)
+
+classificacao = [(e[0], e[1],
+                  'Baixo peso' if e[2] <= 18.5 else 'Peso normal'
+                  if e[2] >= 18.5 and e[2] <= 24.99 else 'Sobrepeso'
+                  if e[2] >= 25 and e[2] <= 29.99 else 'Obesidade')
+                 for e in imcs]
+print(classificacao)
+
+
+def classifica_imc(imc):
+    if imc <= 18.5:
+        return 'Baixo peso'
+    elif imc >= 18.5 and imc <= 24.99:
+        return 'Peso normal'
+    elif imc >= 25 and imc <= 29.99:
+        return 'Sobrepeso'
+    else:
+        return 'Obesidade'
+
+
+classificacao2 = [(cod, nome, classifica_imc(imc))
+                  for cod, nome, imc in imcs]
+print(classificacao2)
