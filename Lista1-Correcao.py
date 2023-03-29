2ea61da8e5ac2ee106986dbaab1dd4211baf0f65
Murilo Scapim <mhscapim@gmail.com>
2023-03-29 13:07:15 -0300

    Aula 28/03

--- Lista1-Correcao.py
+++ Lista1-Correcao.py
@@ -19,12 +19,14 @@ print(f"A resposta do exercício 3 é: {nova_lista}")
 
 # Ex 4
 
+
 def ContVog(nome):
     cont=0
     for letra in nome:
         if letra in "aeiou":
             cont+=1
     return cont
+
 
 ListaVog=[nome for nome in lista if ContVog(nome.lower()) >= 3]
 print(f"A resposta do exercicio 4 é: {ListaVog}")
@@ -34,3 +36,24 @@ print(f"A resposta do exercicio 4 é: {ListaVog}")
 frase = "Sistemas de Informação"
 VogaisFrase = [letra for letra in frase if letra.lower() in "aeiou"]
 print(f"A resposta do exercicio 5 é: {VogaisFrase}")
+
+# Ex 6
+Lista3 = [False, 'Terra', 'Marte', 100, True, 25.4, 'Saturno', 'Urano']
+Lista6R = [e for e in Lista3 if type(e) == str]
+
+# Ex 7
+Lista4 = [1, 2, 3, 4]
+Lista5 = [2, 3, 5, 6]
+
+Lista7R = [e for e in Lista4 if e in Lista5]
+
+# Ex 8
+Lista6 = ['Melão', 'Kiwi', 'Abacaxi', 'Pera']
+Lista8R = [(Lista6.index(fruta), fruta) for fruta in Lista6]
+print(Lista8R)
+
+# Ex 9
+student = [("Batman", 1998), ("Alfred", 1995),
+           ("Jim", 1999), ("Thomas", 2000), ("Bruce", 2003)]
+
+Lista9 = [name for (name, year) in student if year < 2000]
