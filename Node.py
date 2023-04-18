483da1772d85c3ec8af813e7b85cb85119ad4a7a
Murilo Scapim <mhscapim@gmail.com>
2023-04-17 22:15:10 -0300

    Aula 17/04

---
+++ Node.py
@@ -0,0 +1,10 @@
+""" A classe Node é usada para representar
+cada elemento da lista encadeada, contendo um atributo
+para armazenar o dado e um ponteiro para
+o próximo nó da lista"""
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
