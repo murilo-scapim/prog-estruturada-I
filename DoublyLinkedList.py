4097e54d115a070030ab554c3591eb6f58cf7cc2
Murilo Scapim <mhscapim@gmail.com>
2023-05-16 22:14:37 -0300

    Aula 16/05

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -21,3 +21,30 @@ class DoublyLinkedList:
 
             current.next = new_node
             new_node.prev = current
+    
+    # adiciona um elemento no início da lista
+    def prepend(self, data):
+        new_node = DoublyNode(data)
+        if self.is_empty():
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+    
+    def show(self):
+        current = self.head
+        while current is not None:
+            print(current.data)
+            current = current.next
+
+
+doubly_node = DoublyLinkedList()
+doubly_node.append(1)
+doubly_node.append(2)
+doubly_node.append(3)
+doubly_node.prepend(0)
+
+doubly_node.show()
+
+print(doubly_node.is_empty())
